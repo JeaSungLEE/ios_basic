8605503433e517a168ea077d76ce82f8f81a7f1a
kaskay <JoyLee@ijaeseong-ui-MacBook-Pro.local>
2016-03-02 10:14:20 +0900

    week1

---
+++ week1/week1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  week1
+//
+//  Created by 이재성 on 2016. 3. 2..
+//  Copyright © 2016년 이재성. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
